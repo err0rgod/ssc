44d0926d0fb12a73c2e938589bd396e3397aba0d
err0rgod <ndgaming458@gmail.com>
2025-07-27 00:07:38 +0530

    basic initialize

---
+++ basic.py
@@ -0,0 +1,14 @@
+import paramiko
+
+
+host = "sciring.in"
+user = "root"
+password = "wrog"
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    client.connect(hostname=host, username=user , password=password, timeout=3)
+    print(f"the connection was successful with {host}")
+
+except paramiko.AuthenticationException:
+    print(f" the connnection was failed with {host}")
