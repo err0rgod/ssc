587f0e6ad938e663b425aafb3c79e512148fd707
err0rgod <ndgaming458@gmail.com>
2025-07-28 20:13:39 +0530

    wohoooo

--- adv.py
+++ adv.py
@@ -8,3 +8,15 @@ parser = argparse.ArgumentParser(description="Advance SSH Cracker")
 parser.add_argument("-t","--target",required=True,type=str,help="Enter The target")
 parser.add_argument("-u","--user",type=str,required=True,help="Enter the user or a Wordlist to user names")
 parser.add_argument("-p","--passwd",type=str,required=True,help="Enter the path to wordlist containing passwords")
+
+
+
+args = parser.parse_args()
+
+host = args.target
+user = args.user
+passwd = args.passwd
+
+
+
+
