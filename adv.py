30727909696bbbdce3a087276fd7ba1d209015e9
err0rgod <ndgaming458@gmail.com>
2025-07-28 20:16:08 +0530

    check ssh done

--- adv.py
+++ adv.py
@@ -3,20 +3,31 @@ import argparse
 
 
 
-parser = argparse.ArgumentParser(description="Advance SSH Cracker")
+#parser = argparse.ArgumentParser(description="Advance SSH Cracker")
 
-parser.add_argument("-t","--target",required=True,type=str,help="Enter The target")
-parser.add_argument("-u","--user",type=str,required=True,help="Enter the user or a Wordlist to user names")
-parser.add_argument("-p","--passwd",type=str,required=True,help="Enter the path to wordlist containing passwords")
+#parser.add_argument("-t","--target",required=True,type=str,help="Enter The target")
+#parser.add_argument("-u","--user",type=str,required=True,help="Enter the user or a Wordlist to user names")
+#parser.add_argument("-p","--passwd",type=str,required=True,help="Enter the path to wordlist containing passwords")
+#
+
+#
+#args = parser.parse_args()
+#
+#host = args.target
+#user = args.user
+#passwd = args.passwd
+#
 
 
+def check_ssh(ip="steminfinity.in", port=22, timeout=3):
+    try:
+        with socket.create_connection((ip, port), timeout=timeout):
+            
+            print("Success")
+            return True
+    except (socket.timeout, ConnectionRefusedError, OSError):
+        print("Failure")
+        return False
 
-args = parser.parse_args()
 
-host = args.target
-user = args.user
-passwd = args.passwd
-
-
-
-
+check_ssh()
